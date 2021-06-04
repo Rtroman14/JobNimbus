82bd908f0f84b61198e17bbe6316ea6d123f3c42
Ryan Roman <rtroman14@gmail.com>
2021-06-04 11:41:52 -0600

    first commit

---
+++ src/JobNimbus.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+
+const axios = require("axios");
+
+module.exports = class JobNimbus {
+    constructor(token) {
+        this.token = token;
+    }
+
+    getConfig(method, url, data) {
+        try {
+            if (data) {
+                return {
+                    method,
+                    url,
+                    headers: {
+                        Authorization: `Bearer ${this.token}`,
+                        "Content-Type": "application/json",
+                    },
+                    data,
+                };
+            }
+
+            return {
+                method,
+                url,
+                headers: {
+                    Authorization: `Bearer ${this.token}`,
+                    "Content-Type": "application/json",
+                },
+            };
+        } catch (error) {
+            console.log("ERROR CONFIG ---", error);
+        }
+    }
+
+    async contacts() {
+        try {
+            const config = this.getConfig("get", "https://app.jobnimbus.com/api1/contacts");
+
+            const res = await axios(config);
+
+            return res.data.results;
+        } catch (error) {
+            console.log("ERROR CREATECONTACT ---", error);
+        }
+    }
+};
