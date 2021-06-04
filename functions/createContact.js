e18da6375947e929b80cb879cbdaaa580caffc23
Rtroman14 <rtroman14@gmail.com>
2021-06-04 12:03:12 -0600

    update test route

---
+++ functions/createContact.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+exports.handler = async (event) => {
+    if (event.httpMethod === "GET") {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ msg: "POST request only" }),
+        };
+    } else if (event.httpMethod === "POST") {
+        const res = JSON.parse(event.body);
+
+        console.log(res);
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ res }),
+        };
+    } else {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ msg: "Error" }),
+        };
+    }
+};
--- src/JobNimbus.js
+++ src/JobNimbus.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+1require("dotenv").config();
 
 const axios = require("axios");
 
