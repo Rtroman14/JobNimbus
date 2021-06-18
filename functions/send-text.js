57e11a832da97b05653d0968dd1a6029fafc71fa
Ryan Roman <rtroman14@gmail.com>
2021-06-18 15:13:38 -0600

    update

---
+++ functions/send-text.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+exports.handler = async (event) => {
+    if (event.httpMethod === "GET") {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ msg: "POST request only" }),
+        };
+    } else if (event.httpMethod === "POST") {
+        const res = JSON.parse(event.body);
+
+        const { queryStringParameters } = event;
+
+        console.log(res);
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ queryStringParameters }),
+        };
+    } else {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ msg: "Error" }),
+        };
+    }
+};
