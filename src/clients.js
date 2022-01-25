b0785d354ac51dc019f61d2c9bd74e8ccc7dd75e
Rtroman14 <rtroman14@gmail.com>
2022-01-25 12:57:22 -0700

    update

--- src/clients.js
+++ src/clients.js
@@ -19,7 +19,7 @@ module.exports = (client, contact = { Research: "", Tag: "" }) => {
         {
             client: "Eco Tec",
             additionalContactFields: {
-                record_type_name: "Roof Coatings",
+                record_type_name: "Customer",
                 status_name: "Lead",
                 Research: contact["First Line"] || contact["FIRST LINE"] || "",
                 Service: "Roof Coatings",
