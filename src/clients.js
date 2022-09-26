cb671a91edde5fca7e42c1096ecf4825d8beca55
Ryan Roman <rtroman14@gmail.com>
2022-09-26 09:29:37 -0600

    update

--- src/clients.js
+++ src/clients.js
@@ -179,6 +179,33 @@ module.exports = (client, contact = { Research: "", Tag: "" }) => {
                 typeName: "New Lead",
             },
         },
+        {
+            client: "Integrity Pro Roofing",
+            additionalContactFields: {
+                record_type_name: "Commercial Roofing",
+                status_name: "Lead",
+                sales_rep_name: "Rae Boyce",
+                source_name: "Summa Media / Peak Leads",
+            },
+            additionalJobFields: {
+                name: `${contact.Street || contact["Last Name"] + " " + contact["First Name"]}`,
+                status_name: "Lead",
+                sales_rep_name: "Rae Boyce",
+                record_type_name: "",
+                source_name: "Summa Media / Peak Leads",
+                location: 1,
+            },
+            scheduledCall: {
+                salesRep: "", // team member id who will be assigned to the task: New Lead - Follow Up
+                jobStatusName: "",
+            },
+            leadFollowUp: "38t41a", // team member id who will be assigned to the task: New Lead - Follow Up
+            team: [{ name: "Tony Jackson", id: "38t41a" }],
+            task: {
+                title: "New Lead",
+                typeName: "New Lead",
+            },
+        },
     ];
 
     return clients.find((clientObj) => clientObj.client === client);
